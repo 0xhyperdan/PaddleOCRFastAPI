ff1f80560440322c950759c881bd27e34f6db596
Xiaodan Cong <me@cxd.name>
2025-11-26 17:15:15 +0800

    fix

--- routers/ocr.py
+++ routers/ocr.py
@@ -1,38 +1,51 @@
 # -*- coding: utf-8 -*-
 
+import os
+
+import requests
 from fastapi import APIRouter, HTTPException, UploadFile, status
+from paddleocr import PaddleOCR
+
 from models.OCRModel import *
 from models.RestfulModel import *
-from paddleocr import PaddleOCR
 from utils.ImageHelper import base64_to_ndarray, bytes_to_ndarray
-import requests
-import os
 
 OCR_LANGUAGE = os.environ.get("OCR_LANGUAGE", "ch")
 
 router = APIRouter(prefix="/ocr", tags=["OCR"])
 
-ocr = PaddleOCR(use_angle_cls=True, lang=OCR_LANGUAGE)
+ocr = PaddleOCR(
+    use_doc_orientation_classify=False,  # 通过 use_doc_orientation_classify 参数指定不使用文档方向分类模型
+    use_doc_unwarping=False,  # 通过 use_doc_unwarping 参数指定不使用文本图像矫正模型
+    use_textline_orientation=False,  # 通过 use_textline_orientation 参数指定不使用文本行方向分类模型
+    text_detection_model_name="PP-OCRv5_server_det",  # 指定使用 PP-OCRv5 检测模型
+    text_recognition_model_name="PP-OCRv5_server_rec",  # 指定使用 PP-OCRv5 识别模型
+    lang=OCR_LANGUAGE,  # 通过 lang 参数指定使用的语言模型
+)
 
 
-@router.get('/predict-by-path', response_model=RestfulModel, summary="识别本地图片")
+@router.get("/predict-by-path", response_model=RestfulModel, summary="识别本地图片")
 def predict_by_path(image_path: str):
     result = ocr.ocr(image_path, cls=True)
     restfulModel = RestfulModel(
-        resultcode=200, message="Success", data=result, cls=OCRModel)
+        resultcode=200, message="Success", data=result, cls=OCRModel
+    )
     return restfulModel
 
 
-@router.post('/predict-by-base64', response_model=RestfulModel, summary="识别 Base64 数据")
+@router.post(
+    "/predict-by-base64", response_model=RestfulModel, summary="识别 Base64 数据"
+)
 def predict_by_base64(base64model: Base64PostModel):
     img = base64_to_ndarray(base64model.base64_str)
     result = ocr.ocr(img=img, cls=True)
     restfulModel = RestfulModel(
-        resultcode=200, message="Success", data=result, cls=OCRModel)
+        resultcode=200, message="Success", data=result, cls=OCRModel
+    )
     return restfulModel
 
 
-@router.post('/predict-by-file', response_model=RestfulModel, summary="识别上传文件")
+@router.post("/predict-by-file", response_model=RestfulModel, summary="识别上传文件")
 async def predict_by_file(file: UploadFile):
     restfulModel: RestfulModel = RestfulModel()
     if file.filename.endswith((".jpg", ".png")):  # 只处理常见格式图片
@@ -46,17 +59,19 @@ async def predict_by_file(file: UploadFile):
     else:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="请上传 .jpg 或 .png 格式图片"
+            detail="请上传 .jpg 或 .png 格式图片",
         )
     return restfulModel
 
 
-@router.get('/predict-by-url', response_model=RestfulModel, summary="识别图片 URL")
+@router.get("/predict-by-url", response_model=RestfulModel, summary="识别图片 URL")
 async def predict_by_url(imageUrl: str):
     restfulModel: RestfulModel = RestfulModel()
     response = requests.get(imageUrl)
     image_bytes = response.content
-    if image_bytes.startswith(b"\xff\xd8\xff") or image_bytes.startswith(b"\x89PNG\r\n\x1a\n"):  # 只处理常见格式图片 (jpg / png)
+    if image_bytes.startswith(b"\xff\xd8\xff") or image_bytes.startswith(
+        b"\x89PNG\r\n\x1a\n"
+    ):  # 只处理常见格式图片 (jpg / png)
         restfulModel.resultcode = 200
         img = bytes_to_ndarray(image_bytes)
         result = ocr.ocr(img=img, cls=True)
@@ -65,6 +80,6 @@ async def predict_by_url(imageUrl: str):
     else:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
-            detail="请上传 .jpg 或 .png 格式图片"
+            detail="请上传 .jpg 或 .png 格式图片",
         )
     return restfulModel
