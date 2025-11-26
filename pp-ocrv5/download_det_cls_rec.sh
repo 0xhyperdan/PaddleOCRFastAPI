e4cb3fc8623e7c3bd12cd08fad6779a50cce6df1
Xiaodan Cong <me@cxd.name>
2025-11-26 15:59:34 +0800

    update paddle latest version via PP_OCRv5

---
+++ pp-ocrv5/download_det_cls_rec.sh
@@ -0,0 +1,3 @@
+# pp-ocrv5模型下载：
+wget https://paddle-model-ecology.bj.bcebos.com/paddlex/official_inference_model/paddle3.0.0//PP-OCRv5_server_rec_infer.tar
+wget https://paddle-model-ecology.bj.bcebos.com/paddlex/official_inference_model/paddle3.0.0/PP-OCRv5_server_det_infer.tar
